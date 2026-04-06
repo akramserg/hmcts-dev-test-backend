ee21a0e1f4cdb84cbe917f4002c0d29e720836ba
Akram Sergewa <akram.sergewa@gmail.com>
2026-04-06 15:07:37 +0100

    Added endpoints and the calls to the correct service functions

--- src/main/java/uk/gov/hmcts/reform/dev/controllers/TaskController.java
+++ src/main/java/uk/gov/hmcts/reform/dev/controllers/TaskController.java
@@ -1,8 +1,22 @@
 package uk.gov.hmcts.reform.dev.controllers;
 
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.dev.dto.CreateTaskRequest;
+import uk.gov.hmcts.reform.dev.dto.TaskResponse;
+import uk.gov.hmcts.reform.dev.dto.UpdateTaskStatusRequest;
 import uk.gov.hmcts.reform.dev.services.TaskService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
@@ -12,5 +26,33 @@ public class TaskController {
 
     public TaskController(TaskService taskService) {
         this.taskService = taskService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TaskResponse> createTask(@RequestBody @Valid CreateTaskRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskService.createTask(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TaskResponse>> getAllTasks() {
+        return ResponseEntity.ok(taskService.getAllTasks());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskResponse> getTaskById(@PathVariable Long id) {
+        return ResponseEntity.ok(taskService.getTaskById(id));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<TaskResponse> updateTaskStatus(
+        @PathVariable Long id,
+        @RequestBody @Valid UpdateTaskStatusRequest request) {
+        return ResponseEntity.ok(taskService.updateTaskStatus(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return ResponseEntity.noContent().build();
     }
 }
